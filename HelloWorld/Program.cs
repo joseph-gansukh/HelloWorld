137bc8e4075fdfa074b0e8a90f6b7631f50afc51
Joseph S Gansukh <52771685+joseph-gansukh@users.noreply.github.com>
2020-09-20 22:07:38 -0500

    Update Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Learning C#");
+            Console.WriteLine("Learning C#!");
         }
     }
 }
